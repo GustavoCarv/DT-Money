5f643f25ab46b23dfb603b6270af4ffbee09f8bd
GustavoCarv <gustavo.carvg@gmail.com>
2021-12-16 18:10:43 -0300

    add: iniciada funcionalidade de adicionar uma nova transação

--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal from "react-modal";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import entradaImg from "../../assets/Entradas.svg";
 import saidaImg from "../../assets/Saídas.svg";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import {api} from "../../services/api";
 
 interface newTransactionModalProps {
   isOpen: boolean;
@@ -14,6 +15,23 @@ export function NewTransactionModal({
   onRequestClose,
 }: newTransactionModalProps) {
   const [transactionType, setTransactionType] = useState("deposit");
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
+
+  function handleCreateNewTransation(event: FormEvent) {
+    event.preventDefault();
+
+    const newData = {
+      title,
+      value,
+      category,
+      type: transactionType,
+    }
+
+    api.post('/transactions', newData)
+  }
+
 
   return (
     <Modal
@@ -29,10 +47,10 @@ export function NewTransactionModal({
       >
         +
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransation}>
         <h2>Cadastrar Transação</h2>
-        <input placeholder="Titulo" />
-        <input placeholder="Valor" type="number" />
+        <input placeholder="Titulo" value={title} onChange={(event) => {setTitle(event.target.value)}} />
+        <input placeholder="Valor" type="number"  value={value} onChange={(event) => {setValue(Number(event.target.value))}} />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -59,7 +77,7 @@ export function NewTransactionModal({
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input placeholder="Categoria"  value={category} onChange={(event) => {setCategory(event.target.value)}} />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
